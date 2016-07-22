428c55add5ab7b7d35aea58a45b486318028081f
jeremyduanewallace <jeremyduanewallace@gmail.com>
2016-07-22 08:46:01 -0700

    did what I could

---
+++ reddit.js
@@ -0,0 +1,36 @@
+// Jeremy Wallace
+// reddit the dom
+
+// getElementById
+
+var changeHtml = document.getElementById('header-bottom-right');
+undefined
+changeHtml.innerHTML = 'jeremy';
+"jeremy"
+changeHtml.style.backgroundColor = 'yellow';
+"yellow"
+var changeHeader = document.getElementById('header-bottom-left');
+undefined
+changeHeader.innerHTML = 'yellow';
+"yellow"
+changeHeader.style.backgroundColor = 'yellow';
+"yellow"
+changeHtml.style.fontSize = 'bold';
+"bold"
+
+
+// getElementsByClassName
+
+for(var i = 0; i < data.children.lenght; i++){
+		var thumb = data.children[i];
+}
+// getElementByTagName
+
+var linkColor = document.getElementByTagName('a');
+for(var i = 0; i < linkColor[0]; i++){ 
+	if(linkColor[0] == a)
+		
+// queryselecteor
+
+}
+data.children[1].data.author_flair_css_class
