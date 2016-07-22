d216aa469d34c5d494310103a5de72ca879f7c58
shaneehlert <shane@esperas.org>
2016-07-22 08:50:18 -0700

    couldn't finish but got what i could done

---
+++ solution.js
@@ -0,0 +1,16 @@
+console.log("answer 1");
+document.getElementById('header-bottom-left').style.backgroundColor = 'red';
+console.log("answer 2");
+hb.innerHTML = 'HellOSHANE';
+console.log("answer 3");
+for (var i = 0; i < nail.length; i++) {
+  var image = nail[i].getElementsByTagName('img')[0];
+  if(image) {
+    image.height = 20;
+    image.width = 20;
+  }
+}
+20
+console.log("answer 4");
+for(var i = 0; i<links.length; i++){links[i].style.backgroundColor = 'red';}
+"red"
