34f3ee896be51e7d2b8f96be3d647ac595ca1fc3
kedeger <kedeger@uw.edu>
2016-07-22 10:30:43 -0700

    selecting reddit deliverable

---
+++ script.js
@@ -0,0 +1,46 @@
+// // #1
+
+// // Define the header as a variable:
+
+// // var header = document.getElementById("header-bottom-left");
+
+// // Color it lime:
+
+// // header.style.backgroundColor = "lime";  
+// // "lime"
+
+// // Change header text:
+// // document.getElementById("header-bottom-left").innerHTML = "Hello Kemal!";
+// // "Hello Kemal!"
+
+// // Bold the header's text:
+// // document.getElementById("header-bottom-left").style.fontWeight = "900";
+// // "900"
+// ----------------------------------------------------
+// #2
+
+// var thumbnails = document.getElementsByClassName("thumbnail");
+// var i;
+// for (i = 0; i < thumbnails.length; i++) {
+//   if(thumbnails[i].children[0]){
+//     thumbnails[i].children[0].style.height = "20px";
+//     thumbnails[i].children[0].style.width = "20px";
+// }}
+
+// "20px"
+// --------------------------------------------------------
+// #3
+// var links = document.getElementsByTagName('a');
+
+// for(var i = 0; i<links.length; i++){links[i].style.backgroundColor = 'red';}
+
+// ----------------------------------------------------------
+// #4
+
+// var rank = document.querySelector("div.thing.id-t3_4tzzhi.odd.link .score.unvoted");
+
+// for(var i = 0; i < rankOrder.length; i++){
+//   .innerHTML = i;
+// }
+// -----------------------------------------------------------
+// #5
